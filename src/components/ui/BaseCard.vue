4d28f26265c10543e68d3990781e124c304b010c
bonabird <34163166+bonabird@users.noreply.github.com>
2022-03-28 21:22:39 +0200

    please oh please

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  max-width: 40rem;
+  height: 250px;
+  width: 250px;
+}
+</style>
---
+++ src/pages/hero/TheWelcome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section>
+      <base-card>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <h1>Hi</h1>
+        <h1>I'm</h1>
+        <h1>Paolo</h1>
+        <h1>Bonavera</h1>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
